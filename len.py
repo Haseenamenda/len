a3741d9f4a4e04f644e67b6b26246ffee9ab131c
Haseenamenda <41771159+Haseenamenda@users.noreply.github.com>
2018-10-17 16:39:43 +0530

    Update len.py

--- len.py
+++ len.py
@@ -1,12 +1,12 @@
 n=input()
-c=0
-k=0
+j=0
+g=0
 for i in n:
 	if(i.isalpha()):
-		c=c+1
+		j=j+1
 	elif(i.isnumeric()):
-		k=k+1
-if c+k==len(n) and c>0 and k>0:
+		g=g+1
+if j+g==len(n) and j>0 and g>0:
 	print("yes")
 else:
 	print("no")
